e949bc9da5240bd4dbcb25cb594e6818cbc16a47
Ishara Umayanga <154123718+isharaUmayanaga@users.noreply.github.com>
2026-08-08 00:06:56 +0530

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   },
   server: {
-    port: 3000
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  preview: {
+    port: 4173,
+    host: '0.0.0.0'
   },
   resolve: {
-    extensions: ['.js', '.jsx']  
+    extensions: ['.js', '.jsx']
   }
-})
+});
